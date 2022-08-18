ba1cfda083febacfe18fff40c8a63ecc450f6ea3
Bruno Nabarrete <brunonabarrete@hotmail.com>
2022-08-18 15:15:06 -0300

    adiciono exercício 2 - 4.1

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,18 @@
+const a = 250;
+const b = 350;
+
+console.log(a + b);
+
+console.log(a - b);
+
+console.log(a * b);
+
+console.log(a / b);
+
+console.log(a % b);
+
+if (a > b) {
+    console.log(a);
+} else if (b > a) {
+    console.log('o maior é' = b);
+}
