6958e629e788b66b902f904b8486f38d26b4e7a8
Bruno Nabarrete <brunonabarrete@hotmail.com>
2022-08-18 13:52:14 -0300

    Exercicios conteúdo 4.1

--- fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/js-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/js-primeiros-passos/scripts.js
@@ -24,3 +24,76 @@ let perimeter = 3 + 3 + 3;
 console.log(perimeter);
 
 
+// operadores de condições If/else;
+
+
+let nota = 50
+nota = 90
+
+if (nota >= 80) {
+    console.log('Parabéns, você foi aprovada');
+} else if (nota < 80 && nota >= 60) {
+    console.log('Você está na nossa lista de espera')
+} else if (nota < 60) {
+    console.log('Você foi reprovado')
+} else {
+
+}
+
+// operadores logicos;
+// operador and;
+
+
+const comida = 'pão na chapa';
+const bebida = 'cafezinho';
+
+if (bebida === 'cafezinho' && comida === 'pão na chapa') {
+    console.log('Muito obrigado pela refeição :)');
+} else {
+    console.log('Acho que houve um engano no meu pedido')
+}
+
+
+const currentHour = 18;
+let message = ''
+
+if (currentHour >= 22) {
+    message = 'Não deveríamos comer nada, é hora de dormir';
+} else if (currentHour >= 18 && currentHour < 22) {
+    message = 'Rango da noite, vamos jantar';
+} else if (currentHour >= 14 && currentHour < 18) {
+    message = 'Vamos fazer um volo pro café da tarde?';
+} else if (currentHour >= 11 && currentHour < 14) {
+    message = 'Hora do almoço!';
+} else if (currentHour >= 4 && currentHour < 11) {
+    message = 'Hm, cheiro de café recém passado'
+}
+
+console.log(message);
+
+// operador OR;
+
+let weekDay = 'sabado';
+
+if (weekDay === 'segunda-feira' || weekDay ==='terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+    console.log('Oba, mais um dia de apredizado na trybe');
+} else {
+    console.log('Finalmente, descanso merecido');
+}
+
+// operador switch -- case;
+
+let estadoCandidato = 'lista';
+
+switch (estadoCandidato) {
+    case 'aprovada':
+        console.log('Aprovado !')
+        break;
+    case 'lista':
+        console.log('Lista de espera');
+        break;
+    case 'reprovada':
+        console.log('Reprovado!');
+    default:
+        console.log('Não se aplica')
+}
