6bd3d0f06c7588dfdbc4d87a5786ee0e71598c53
Bruno Nabarrete <brunonabarrete@hotmail.com>
2022-08-17 22:08:23 -0300

    Inicio do bloco 4, dia 1 - exercicios junto ao conteúdo

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/js-primeiros-passos/scripts.js
@@ -0,0 +1,11 @@
+const myName = 'Bruno Nabarrete da Silva';
+const birthCity = 'São Bernardo do Campo';
+let birthYear = 1997;
+birthYear = 2030
+birthCity = 'Santo André'
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+/* Explicando item 06 do Primeiro Passo: não houve alteração no birthCity, pois se trata de uma constante *\
