93ab356a6a0d62f3e3792f60f7a94294d0fee661
Bruno Nabarrete <brunonabarrete@hotmail.com>
2022-08-17 22:08:57 -0300

    Exercicios de conteudo

--- fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/js-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/js-primeiros-passos/scripts.js
@@ -8,4 +8,19 @@ console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
 
-/* Explicando item 06 do Primeiro Passo: não houve alteração no birthCity, pois se trata de uma constante *\
+// Explicando item 06 do Primeiro Passo: não houve alteração no birthCity, pois se trata de uma constante;
+
+
+let base = 5;
+
+let height = 8;
+
+const area = base * height;
+
+console.log(area);
+
+let perimeter = 3 + 3 + 3;
+
+console.log(perimeter);
+
+
