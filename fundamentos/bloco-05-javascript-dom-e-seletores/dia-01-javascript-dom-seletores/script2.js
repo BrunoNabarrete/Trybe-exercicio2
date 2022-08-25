4189baea6e03a0c525f27984e7bd4cc349014160
Bruno Nabarrete <brunonabarrete@hotmail.com>
2022-08-25 19:28:52 -0300

    adiciono exercicios de aula

---
+++ fundamentos/bloco-05-javascript-dom-e-seletores/dia-01-javascript-dom-seletores/script2.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById("page-title").innerText = 'Filme favorito Vingadores');
+
+document.getElementById("paragraph").innerText = 'Nova atribuição para teste no exercício!'
+
+document.getElementsByClassName('classtest')[0].innerText = 'novo paragrafo para ele como outro teste de class'
+
+document.getElementsByTagName
