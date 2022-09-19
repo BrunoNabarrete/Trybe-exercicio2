88d5ee78643d4ac9d22e84a17b2536be5105d7d2
Bruno Nabarrete <brunonabarrete@hotmail.com>
2022-09-18 21:12:21 -0300

    adiciono exercicio 2 do dia

--- fundamentos/bloco-07-javascript-es6-testes-unitario/dia-01-javascript-let-const-arrow/script.js
+++ fundamentos/bloco-07-javascript-es6-testes-unitario/dia-01-javascript-let-const-arrow/script.js
@@ -1,12 +1,22 @@
-const testingScope = (escopo) => {
-    if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-      console.log(ifScope);
-    } else {
-      let elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
-      console.log(elseScope);
-    }
-  }
+// const testingScope = (escopo) => {
+//     if (escopo === true) {
+//       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//       console.log(ifScope);
+//     } else {
+//       let elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//   }
 
-  testingScope(true);
+//   testingScope(true);
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  const sortOddsAndEvens = () => {
+    oddsAndEvens.sort((a, b) => a - b);
+
+//   console.log(oddsAndEvens);
+  }
+  sortOddsAndEvens();
+  console.log(`Os números ${oddsAndEvens} se encontram ordenador de forma crescente`);
