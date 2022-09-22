9d8a1824e63f2a81e62fcb5ceb98041b9bb1ba0e
Bruno Nabarrete <brunonabarrete@hotmail.com>
2022-09-22 15:18:01 -0300

    adiciono exercicio 3 do dia

--- fundamentos/bloco-08-javascriptES6-introducao-a-Higher-Order_functions/dia-01-introducao-a-Higher-Order/script2.js
+++ fundamentos/bloco-08-javascriptES6-introducao-a-Higher-Order_functions/dia-01-introducao-a-Higher-Order/script2.js
@@ -21,8 +21,8 @@ const randomNumber = () => {
     return Math.floor(Math.random() * 5) + 1;
 }
 
-const checkNumber = (randomNumber) => {
-    const number2 = Math.floor(Math.random() * 5) + 1;
+const checkNumber = () => {
+    const number2 = randomNumber();
     if (randomNumber() === number2) {
         return 'Parabéns você ganhou';
     } 
@@ -30,3 +30,30 @@ const checkNumber = (randomNumber) => {
 }
 
 console.log(checkNumber(randomNumber));
+
+//exercicio 3.
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const compareAnswers = (rightAnswer, studenteAnswer) => {
+    if (rightAnswer === studenteAnswer) {
+        return 1;
+    }
+    if (studenteAnswer === 'N.A') {
+        return 0;
+    }
+    return -0.5;
+}
+
+const countPoints = (rightAnswers, studenteAnswers, action) => {
+    let count = 0;
+    for (let index = 0; index < rightAnswers.length ; index +=1) {
+        const actionReturn = action(rightAnswers[index], studenteAnswers[index]);
+        count += actionReturn;
+    }
+    return `Resultado final: ${count} pontos`
+}
+
+console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
+
