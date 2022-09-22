36cb16e467ddc46d85609bba130099ea63e0c1bd
Bruno Nabarrete <brunonabarrete@hotmail.com>
2022-09-22 14:49:52 -0300

    adiciono exercicio 2

--- fundamentos/bloco-08-javascriptES6-introducao-a-Higher-Order_functions/dia-01-introducao-a-Higher-Order/script2.js
+++ fundamentos/bloco-08-javascriptES6-introducao-a-Higher-Order_functions/dia-01-introducao-a-Higher-Order/script2.js
@@ -1,5 +1,5 @@
 
-
+//exercicio 1.
 const creatLogin = (fullName) => {
     const email = fullName.toLowerCase().split(' ').join('_');
     return {fullName, email: `${email}@trybe.com`};
@@ -14,3 +14,19 @@ const newEmployees = (creatLogin) => {
     return empregados;
 }
 console.log(newEmployees(creatLogin));
+
+//exercicio 2.
+
+const randomNumber = () => {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+const checkNumber = (randomNumber) => {
+    const number2 = Math.floor(Math.random() * 5) + 1;
+    if (randomNumber() === number2) {
+        return 'Parabéns você ganhou';
+    } 
+    return 'Tente novamente';
+}
+
+console.log(checkNumber(randomNumber));
