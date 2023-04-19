12a79bcb217aa34fde80b4c5eff9eff430a7514a
Bruno Nabarrete <brunonabarrete@hotmail.com>
2023-04-19 11:59:57 -0300

    adiciono exercicios do dia

--- back-end/back-end-secao-7.1/exercises/index.ts
+++ back-end/back-end-secao-7.1/exercises/index.ts
@@ -1,4 +1,4 @@
-// ./index.ts
+
 
 import * as Ex from './exercises';
 
---
+++ back-end/back-end-secao-7.1/teste.ts
@@ -0,0 +1,13 @@
+let yes: boolean = true;
+let no: boolean = false;
+
+let x: number;
+
+let y: number = 0;
+let z: number = 10;
+
+
+//void apenas para determinar que não tem um valor, como uma função sem valor retornado.
+function sayHelloWord(): void {
+    console.log('hello word');
+}
