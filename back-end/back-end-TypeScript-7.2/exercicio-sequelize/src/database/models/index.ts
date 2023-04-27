f0d7cbd3a4246e5e54ed65fc79d4fc29370098b4
Bruno Nabarrete <brunonabarrete@hotmail.com>
2023-04-26 22:25:51 -0300

    correção

--- back-end/back-end-TypeScript-7.2/exercicio-sequelize/src/database/models/index.ts
+++ back-end/back-end-TypeScript-7.2/exercicio-sequelize/src/database/models/index.ts
@@ -1,22 +1,26 @@
-import sequelize from 'sequelize';
+import { Sequelize } from 'sequelize';
+import * as config from '../config/database';
 import Author from './AuthorModel';
 import Book from './BookModel';
 
-(async () => {
-  const data = await Author.findAll({
-    include: {
-      model: Book,
-      attributes: [],
-    },
-    attributes: [
-      ['name', 'author'],
-      [sequelize.fn('COUNT', sequelize.col('books.id')), 'totalBooks'],
-    ],
-    group: 'authors.name',
-    order: [['totalBooks', 'DESC']]
-    raw: true,
-  });
 
-  console.log(data);
-  process.exit(0);
-})();
+(async () => {
+    const data = await Author.findAll({
+      include: {
+        model: Book,
+        attributes: [],
+      },
+      attributes: [
+        ['name', 'author'],
+        [Sequelize.fn('COUNT', Sequelize.col('books.id')), 'totalBooks'],
+      ],
+      group: 'authors.name',
+      order: [['totalBooks', 'DESC']],
+      raw: true,
+    });
+  
+    console.log(data);
+    process.exit(0);
+  })();
+
+export default new Sequelize(config);
