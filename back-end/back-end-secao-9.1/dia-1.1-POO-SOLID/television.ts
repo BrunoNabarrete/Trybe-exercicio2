de07ff135dd32330bcfef6e21c802872c5a4d0c3
Bruno Nabarrete <brunonabarrete@hotmail.com>
2023-05-04 00:15:34 -0300

    adiciono encapsulamento

--- back-end/back-end-secao-9.1/dia-1.1-POO-SOLID/television.ts
+++ back-end/back-end-secao-9.1/dia-1.1-POO-SOLID/television.ts
@@ -1,23 +1,40 @@
 class Tv {
-    brand: string;
-    size: number;
-    resolution: string;
-    conection: string[];
-    connectedTo:string;
+   private _brand: string;
+   private _size: number;
+   private _resolution: string;
+   private _conection: string[];
+   private _connectedTo?:string;
 
     constructor(b:string, s: number, r: string, c:string[]) {
         console.log('Criado a tv')
-        this.brand = b;
-        this.size = s;
-        this.resolution = r;
-        this.conection = c;
+        this._brand = b;
+        this._size = s;
+        this._resolution = r;
+        this._conection = c;
+    }
+
+    get connectedTo(): string | undefined {
+        return this._connectedTo;
+    }
+
+    set connectedTo(value: string | undefined) {
+        if (!value || this._conection.includes(value)) {
+           this._connectedTo = value;
+        }else {
+            console.log('Sorry, connection unavailable!')
+        }
+
     }
 
     turnOn () {
-        console.log(`A TV ${this.brand}, do tamanho ${this.size}, com a resolução ${this.resolution}, e com coneção ${this.conection}`);
+        console.log(`A TV ${this._brand}, do tamanho ${this._size}, com a resolução ${this._resolution}, e com coneção ${this._conection}`);
     }
 }
 
 const tv1 = new Tv('LG', 47, '4K', ['HDMI', 'INTERNET', 'WI-FI']);
 
 tv1.turnOn()
+
+tv1.connectedTo = 'WI-FI';
+
+console.log(tv1.connectedTo);
