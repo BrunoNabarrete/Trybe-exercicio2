56f109d62c644dda0fba850d65b95dbab2d14fbe
Bruno Nabarrete <brunonabarrete@hotmail.com>
2023-05-31 19:13:21 -0300

    adicionando a model

---
+++ back-end/back-end-ORM/exercicio-do-dia/src/models/book.model.js
@@ -0,0 +1,9 @@
+const BookModel = (sequelize, DataTypes) => {
+    const Book = sequelize.define('Book', {
+        title: DataTypes.STRING,
+        author: DataTypes.STRING,
+        pageQuantity: DataTypes.INTEGER,
+        createdAt: DataTypes.DATE,
+        updateAt: DataTypes.DATE
+    })
+}
