10be76c4fec12c13a4d1b07b76864ac33beee1d5
Bruno Nabarrete <brunonabarrete@hotmail.com>
2023-07-07 19:26:21 -0300

    adiciono o final

--- back-end/back-end-secao-9.1/exercicio-de-aula-dia1/Order.ts
+++ back-end/back-end-secao-9.1/exercicio-de-aula-dia1/Order.ts
@@ -2,61 +2,73 @@
 import Client from './Clients';
 import OrderItem from './OrderItem';
 
-export default class Order {
-  private _client: Client;
-  private _items: OrderItem[] = [];
-  private _paymentMethod: string;
-  private _discount = 0;
+    export default class Order {
+    private _client: Client;
+    private _items: OrderItem[] = [];
+    private _paymentMethod: string;
+    private _discount = 0;
 
-  constructor(
-    client: Client, 
-    items: OrderItem[], 
-    paymentMethod: string, 
-    discount: number,
-  ) {
-    this._client = client;
-    this.items = items;
-    this._paymentMethod = paymentMethod;
-    this.discount = discount;
-  }
-
-  get client(): Client {
-    return this._client;
-  }
-
-  set client(value: Client) {
-    this._client = value;
-  }
-
-  get items(): OrderItem[] {
-    return this._items;
-  }
-
-  set items(value: OrderItem[]) {
-    if (value.length === 0) {
-      throw new Error('O pedido deve ter pelo meno um item.');
+    constructor(
+        client: Client, 
+        items: OrderItem[], 
+        paymentMethod: string, 
+        discount: number,
+    ) {
+        this._client = client;
+        this.items = items;
+        this._paymentMethod = paymentMethod;
+        this.discount = discount;
     }
 
-    this._items = value;
-  }
-
-  get paymentMethod(): string {
-    return this._paymentMethod;
-  }
-
-  set paymentMethod(value: string) {
-    this._paymentMethod = value;
-  }
-
-  get discount(): number {
-    return this._discount;
-  }
-
-  set discount(value: number) {
-    if (value < 0) {
-      throw new Error('O desconto não pode ser um valor negativo.');
+    get client(): Client {
+        return this._client;
     }
 
-    this._discount = value;
-  }
+    set client(value: Client) {
+        this._client = value;
+    }
+
+    get items(): OrderItem[] {
+        return this._items;
+    }
+
+    set items(value: OrderItem[]) {
+        if (value.length === 0) {
+        throw new Error('O pedido deve ter pelo meno um item.');
+        }
+
+        this._items = value;
+    }
+
+    get paymentMethod(): string {
+        return this._paymentMethod;
+    }
+
+    set paymentMethod(value: string) {
+        this._paymentMethod = value;
+    }
+
+    get discount(): number {
+        return this._discount;
+    }
+
+    set discount(value: number) {
+        if (value < 0) {
+        throw new Error('O desconto não pode ser um valor negativo.');
+        }
+
+        this._discount = value;
+    }
+
+    calculateTotal():number {
+        return this.items.reduce((previusValue, item) => {
+            const total = previusValue + item.price;
+
+            return total
+        }, 0)
+    }
+
+    calculateDiscount(): number {
+        return this.calculateTotal() * (1 - this._discount);
+    }
 }
