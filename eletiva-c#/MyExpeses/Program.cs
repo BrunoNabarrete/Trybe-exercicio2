64977b695903715342ca60a42b2ebfb14c4f48a2
brunoNabarrete <brunonabarrete@hotmail.com>
2023-09-22 18:57:45 -0300

    adiciono e finalizo exercicios do dia

---
+++ eletiva-c#/MyExpeses/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+  public double CalculateBMI(int weight, int height)
+  {
+    return weight / (height * height);
+  }
+
+  public int CalculateAge(int yearOfBirth)
+  {
+    return DateTime.Now.Year - yearOfBirth;
+  }
+}
